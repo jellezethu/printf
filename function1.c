1396a5fc399c5bfe84fb28f20c96b19deed9a550
Zethu <jellezethu@gmail.com>
2023-06-28 07:11:18 -0700

    commit

--- function1.c
+++ function1.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 
-/************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
  * @types: Lista of arguments
@@ -12,6 +11,8 @@
  * @size: Size specifier
  * Return: Number of chars printed
  */
+
+
 int print_unsigned(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
@@ -30,14 +31,14 @@ int print_unsigned(va_list types, char buffer[],
 		buffer[i--] = (num % 10) + '0';
 		num /= 10;
 	}
+	if (flags & F_HASH && init_num != 0)
+		buffer[i--] = '0';
 
 	i++;
 
-	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
-}
+	return (write_unsgnd(0, i, buffer, flags, width, precision,size));
 
-/************* PRINT UNSIGNED NUMBER IN OCTAL  ****************/
-/**
+/*
  * print_octal - Prints an unsigned number in octal notation
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
@@ -47,10 +48,10 @@ int print_unsigned(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
+
 int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
 {
-
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
@@ -78,9 +79,8 @@ int print_octal(va_list types, char buffer[],
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
 
-/************** PRINT UNSIGNED NUMBER IN HEXADECIMAL **************/
 /**
- * print_hexadecimal - Prints an unsigned number in hexadecimal notation
+ * print_hexadecimal - Prints an unsigned number in hexad notation
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
@@ -96,7 +96,6 @@ int print_hexadecimal(va_list types, char buffer[],
 		flags, 'x', width, precision, size));
 }
 
-/************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
 /**
  * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
  * @types: Lista of arguments
@@ -114,7 +113,7 @@ int print_hexa_upper(va_list types, char buffer[],
 		flags, 'X', width, precision, size));
 }
 
-/************** PRINT HEXX NUM IN LOWER OR UPPER **************/
+
 /**
  * print_hexa - Prints a hexadecimal number in lower or upper
  * @types: Lista of arguments
